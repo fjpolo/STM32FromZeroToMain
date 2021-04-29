dbf4918a42bb0c24269672272597b909cc417328
franco.polo <franco.polo@ii-vi.com>
2021-04-29 11:43:11 +0200

    Added PyCortexMDebug and Reverse Engineering.-

---
+++ ReverseEngineering/radare/include/r_version.h
@@ -0,0 +1,13 @@
+#ifndef R_VERSION_H
+#define R_VERSION_H 1
+#define R_MESON_VERSION "0.53.2"
+#define R2_VERSION_MAJOR 4
+#define R2_VERSION_MINOR 3
+#define R2_VERSION_PATCH 1
+#define R2_VERSION_NUMBER 40301
+#define R2_VERSION_COMMIT 6
+#define R2_VERSION "4.3.1"
+#define R2_GITTAP "4.3.1"
+#define R2_GITTIP "54ac837b5503f10f91e2069ac357791f7a3e635a"
+#define R2_BIRTH "Fri 03/06/2020__15:52:24.93"
+#endif
